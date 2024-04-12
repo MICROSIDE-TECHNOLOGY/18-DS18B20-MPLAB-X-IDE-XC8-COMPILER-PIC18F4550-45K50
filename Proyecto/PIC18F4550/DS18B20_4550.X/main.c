1e4fd036467db6b034034eb034224bcd69f5eb27
vhperaltaj <victor.peralta@microside.com>
2024-04-12 13:02:37 -0600

    Improved formatting

--- Proyecto/PIC18F4550/DS18B20_4550.X/main.c
+++ Proyecto/PIC18F4550/DS18B20_4550.X/main.c
@@ -6,98 +6,97 @@
  */
 
 // CONFIG1L
-#pragma config PLLDIV = 1       // PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
-#pragma config CPUDIV = OSC1_PLL2// System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
-#pragma config USBDIV = 1       // USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes directly from the primary oscillator block with no postscale)
+#pragma config PLLDIV = 1         // PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
+#pragma config CPUDIV = OSC1_PLL2 // System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
+#pragma config USBDIV = 1         // USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes directly from the primary oscillator block with no postscale)
 
 // CONFIG1H
-#pragma config FOSC = HSPLL_HS  // Oscillator Selection bits (HS oscillator, PLL enabled (HSPLL))
-#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
-#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+#pragma config FOSC = HSPLL_HS // Oscillator Selection bits (HS oscillator, PLL enabled (HSPLL))
+#pragma config FCMEN = OFF     // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF      // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
 
 // CONFIG2L
-#pragma config PWRT = ON        // Power-up Timer Enable bit (PWRT enabled)
-#pragma config BOR = OFF        // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
-#pragma config BORV = 3         // Brown-out Reset Voltage bits (Minimum setting 2.05V)
-#pragma config VREGEN = OFF     // USB Voltage Regulator Enable bit (USB voltage regulator disabled)
+#pragma config PWRT = ON    // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOR = OFF    // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
+#pragma config BORV = 3     // Brown-out Reset Voltage bits (Minimum setting 2.05V)
+#pragma config VREGEN = OFF // USB Voltage Regulator Enable bit (USB voltage regulator disabled)
 
 // CONFIG2H
-#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
-#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+#pragma config WDT = OFF     // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 32768 // Watchdog Timer Postscale Select bits (1:32768)
 
 // CONFIG3H
-#pragma config CCP2MX = ON      // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
-#pragma config PBADEN = OFF     // PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
-#pragma config LPT1OSC = OFF    // Low-Power Timer 1 Oscillator Enable bit (Timer1 configured for higher power operation)
-#pragma config MCLRE = ON       // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+#pragma config CCP2MX = ON   // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = OFF  // PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+#pragma config LPT1OSC = OFF // Low-Power Timer 1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config MCLRE = ON    // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
 
 // CONFIG4L
-#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
-#pragma config LVP = OFF         // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
-#pragma config ICPRT = OFF      // Dedicated In-Circuit Debug/Programming Port (ICPORT) Enable bit (ICPORT disabled)
-#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+#pragma config STVREN = ON // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = OFF   // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
+#pragma config ICPRT = OFF // Dedicated In-Circuit Debug/Programming Port (ICPORT) Enable bit (ICPORT disabled)
+#pragma config XINST = OFF // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
 
 // CONFIG5L
-#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-001FFFh) is not code-protected)
-#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) is not code-protected)
-#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) is not code-protected)
-#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) is not code-protected)
+#pragma config CP0 = OFF // Code Protection bit (Block 0 (000800-001FFFh) is not code-protected)
+#pragma config CP1 = OFF // Code Protection bit (Block 1 (002000-003FFFh) is not code-protected)
+#pragma config CP2 = OFF // Code Protection bit (Block 2 (004000-005FFFh) is not code-protected)
+#pragma config CP3 = OFF // Code Protection bit (Block 3 (006000-007FFFh) is not code-protected)
 
 // CONFIG5H
-#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) is not code-protected)
-#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM is not code-protected)
+#pragma config CPB = OFF // Boot Block Code Protection bit (Boot block (000000-0007FFh) is not code-protected)
+#pragma config CPD = OFF // Data EEPROM Code Protection bit (Data EEPROM is not code-protected)
 
 // CONFIG6L
-#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-001FFFh) is not write-protected)
-#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) is not write-protected)
-#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) is not write-protected)
-#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) is not write-protected)
+#pragma config WRT0 = OFF // Write Protection bit (Block 0 (000800-001FFFh) is not write-protected)
+#pragma config WRT1 = OFF // Write Protection bit (Block 1 (002000-003FFFh) is not write-protected)
+#pragma config WRT2 = OFF // Write Protection bit (Block 2 (004000-005FFFh) is not write-protected)
+#pragma config WRT3 = OFF // Write Protection bit (Block 3 (006000-007FFFh) is not write-protected)
 
 // CONFIG6H
-#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) are not write-protected)
-#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block (000000-0007FFh) is not write-protected)
-#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM is not write-protected)
+#pragma config WRTC = OFF // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) are not write-protected)
+#pragma config WRTB = OFF // Boot Block Write Protection bit (Boot block (000000-0007FFh) is not write-protected)
+#pragma config WRTD = OFF // Data EEPROM Write Protection bit (Data EEPROM is not write-protected)
 
 // CONFIG7L
-#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-001FFFh) is not protected from table reads executed in other blocks)
-#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) is not protected from table reads executed in other blocks)
-#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) is not protected from table reads executed in other blocks)
-#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR0 = OFF // Table Read Protection bit (Block 0 (000800-001FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF // Table Read Protection bit (Block 1 (002000-003FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF // Table Read Protection bit (Block 2 (004000-005FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF // Table Read Protection bit (Block 3 (006000-007FFFh) is not protected from table reads executed in other blocks)
 
 // CONFIG7H
-#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) is not protected from table reads executed in other blocks)
+#pragma config EBTRB = OFF // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) is not protected from table reads executed in other blocks)
 
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
 #include <xc.h>
 #define _XTAL_FREQ 48000000
-#define F_CPU 48000000/64
-
+#define F_CPU 48000000 / 64
 
 #include "DS18x20.h"
 #include "lcd.h"
-#include <string.h>
 #include <stdint.h>
 #include <stdio.h>
-
+#include <string.h>
 
 char data_in;
 
-void USART_Init(long baud_rate);
-void USART_TransmitChar(char out);
+void USART_Init( long baud_rate );
+void USART_TransmitChar( char out );
 char USART_ReceiveChar();
-void USART_SendString(const char *out);
+void USART_SendString( const char *out );
 
-void main(void) {
+void main( void )
+{
     ADCON1bits.PCFG = 0xFF;
     TRISAbits.RA1 = 0;
     LATA = 0;
     TRISB = 0;
     LATB = 0;
-    __delay_ms(200);
+    __delay_ms( 200 );
     Lcd_Init();
-    __delay_ms(100);
-    while (1) {
+    __delay_ms( 100 );
+    while ( 1 ) {
 
         uint8_t i;
         uint8_t present = 0;
@@ -106,57 +105,58 @@ void main(void) {
         uint8_t addr[8];
         float celsius, fahrenheit;
 
-        if (!search(addr)) {
+        if ( !search( addr ) ) {
             reset_search();
-            __delay_ms(250);
+            __delay_ms( 250 );
             return;
         }
 
         reset();
-        select(addr);
-        write(0x44); // start conversion, with parasite power on at the end
+        select( addr );
+        write( 0x44 );      // start conversion, with parasite power on at the end
 
-        __delay_ms(1000); // maybe 750ms is enough, maybe not
+        __delay_ms( 1000 ); // maybe 750ms is enough, maybe not
         // we might do a ds.depower() here, but the reset will take care of it.
 
         present = reset();
-        select(addr);
-        write(0xBE); // Read Scratchpad
-
-
+        select( addr );
+        write( 0xBE );              // Read Scratchpad
 
         char HEX_[10];
-        for (i = 0; i < 9; i++) { // we need 9 bytes
+        for ( i = 0; i < 9; i++ ) { // we need 9 bytes
             data[i] = read();
-            sprintf(HEX_, "0x%x", data[i]);
+            sprintf( HEX_, "0x%x", data[i] );
         }
 
         // Convert the data to actual temperature
         // because the result is a 16 bit signed integer, it should
         // be stored to an "int16_t" type, which is always 16 bits
         // even when compiled on a 32 bit processor.
-        int16_t raw = (data[1] << 8) | data[0];
-        if (type_s) {
+        int16_t raw = ( data[1] << 8 ) | data[0];
+        if ( type_s ) {
             raw = raw << 3; // 9 bit resolution default
-            if (data[7] == 0x10) {
+            if ( data[7] == 0x10 ) {
                 // "count remain" gives full 12 bit resolution
-                raw = (raw & 0xFFF0) + 12 - data[6];
+                raw = ( raw & 0xFFF0 ) + 12 - data[6];
             }
         } else {
-            uint8_t cfg = (data[4] & 0x60);
+            uint8_t cfg = ( data[4] & 0x60 );
             // at lower res, the low bits are undefined, so let's zero them
-            if (cfg == 0x00) raw = raw & ~7; // 9 bit resolution, 93.75 ms
-            else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-            else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+            if ( cfg == 0x00 )
+                raw = raw & ~7; // 9 bit resolution, 93.75 ms
+            else if ( cfg == 0x20 )
+                raw = raw & ~3; // 10 bit res, 187.5 ms
+            else if ( cfg == 0x40 )
+                raw = raw & ~1; // 11 bit res, 375 ms
             //// default is 12 bit resolution, 750 ms conversion time
         }
-        celsius = (float) raw / 16.0;
+        celsius = (float)raw / 16.0;
         char Cel_[20];
-        sprintf(Cel_, "Temp: %0.2fC", celsius);
+        sprintf( Cel_, "Temp: %0.2fC", celsius );
 
         Lcd_Clear();
-        Lcd_Set_Cursor(1, 1);
-        Lcd_Write_String(Cel_);
-        __delay_ms(3000);
+        Lcd_Set_Cursor( 1, 1 );
+        Lcd_Write_String( Cel_ );
+        __delay_ms( 3000 );
     }
 }
--- Proyecto/PIC18F45K50/DS18B20_45K50.X/main.c
+++ Proyecto/PIC18F45K50/DS18B20_45K50.X/main.c
@@ -19,25 +19,25 @@
  */
 
 /*
-    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
-    
-    Subject to your compliance with these terms, you may use Microchip software and any 
-    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
-    license terms applicable to your use of third party software (including open source software) that 
+    (c) 2018 Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip software and any
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party
+    license terms applicable to your use of third party software (including open source software) that
     may accompany Microchip software.
-    
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
-    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS
     FOR A PARTICULAR PURPOSE.
-    
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
-    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
-    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
-    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
-    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS
     SOFTWARE.
  */
 
@@ -47,14 +47,14 @@
                          Main application
  */
 
-
 #include "DS18x20.h"
 #include "lcd.h"
-#include <string.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
-void main(void) {
+void main( void )
+{
     // Initialize the device
     SYSTEM_Initialize();
 
@@ -63,20 +63,20 @@ void main(void) {
     // Use the following macros to:
 
     // Enable the Global Interrupts
-    //INTERRUPT_GlobalInterruptEnable();
+    // INTERRUPT_GlobalInterruptEnable();
 
     // Disable the Global Interrupts
-    //INTERRUPT_GlobalInterruptDisable();
+    // INTERRUPT_GlobalInterruptDisable();
 
     // Enable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptEnable();
+    // INTERRUPT_PeripheralInterruptEnable();
 
     // Disable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptDisable();
-    __delay_ms(200);
+    // INTERRUPT_PeripheralInterruptDisable();
+    __delay_ms( 200 );
     Lcd_Init();
-    __delay_ms(100);
-    while (1) {
+    __delay_ms( 100 );
+    while ( 1 ) {
 
         uint8_t i;
         uint8_t present = 0;
@@ -85,58 +85,59 @@ void main(void) {
         uint8_t addr[8];
         float celsius;
 
-        if (!search(addr)) {
+        if ( !search( addr ) ) {
             reset_search();
-            __delay_ms(250);
+            __delay_ms( 250 );
             return;
         }
 
         reset();
-        select(addr);
-        write(0x44); // start conversion, with parasite power on at the end
+        select( addr );
+        write( 0x44 );      // start conversion, with parasite power on at the end
 
-        __delay_ms(1000); // maybe 750ms is enough, maybe not
+        __delay_ms( 1000 ); // maybe 750ms is enough, maybe not
         // we might do a ds.depower() here, but the reset will take care of it.
 
         present = reset();
-        select(addr);
-        write(0xBE); // Read Scratchpad
-
-
+        select( addr );
+        write( 0xBE );              // Read Scratchpad
 
         char HEX_[10];
-        for (i = 0; i < 9; i++) { // we need 9 bytes
+        for ( i = 0; i < 9; i++ ) { // we need 9 bytes
             data[i] = read();
-            sprintf(HEX_, "0x%x", data[i]);
+            sprintf( HEX_, "0x%x", data[i] );
         }
 
         // Convert the data to actual temperature
         // because the result is a 16 bit signed integer, it should
         // be stored to an "int16_t" type, which is always 16 bits
         // even when compiled on a 32 bit processor.
-        int16_t raw = (data[1] << 8) | data[0];
-        if (type_s) {
+        int16_t raw = ( data[1] << 8 ) | data[0];
+        if ( type_s ) {
             raw = raw << 3; // 9 bit resolution default
-            if (data[7] == 0x10) {
+            if ( data[7] == 0x10 ) {
                 // "count remain" gives full 12 bit resolution
-                raw = (raw & 0xFFF0) + 12 - data[6];
+                raw = ( raw & 0xFFF0 ) + 12 - data[6];
             }
         } else {
-            uint8_t cfg = (data[4] & 0x60);
+            uint8_t cfg = ( data[4] & 0x60 );
             // at lower res, the low bits are undefined, so let's zero them
-            if (cfg == 0x00) raw = raw & ~7; // 9 bit resolution, 93.75 ms
-            else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-            else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+            if ( cfg == 0x00 )
+                raw = raw & ~7; // 9 bit resolution, 93.75 ms
+            else if ( cfg == 0x20 )
+                raw = raw & ~3; // 10 bit res, 187.5 ms
+            else if ( cfg == 0x40 )
+                raw = raw & ~1; // 11 bit res, 375 ms
             //// default is 12 bit resolution, 750 ms conversion time
         }
-        celsius = (float) raw / 16.0;
+        celsius = (float)raw / 16.0;
         char Cel_[20];
-        sprintf(Cel_, "Temp: %0.2fC", celsius);
+        sprintf( Cel_, "Temp: %0.2fC", celsius );
 
         Lcd_Clear();
-        Lcd_Set_Cursor(1, 1);
-        Lcd_Write_String(Cel_);
-        __delay_ms(3000);
+        Lcd_Set_Cursor( 1, 1 );
+        Lcd_Write_String( Cel_ );
+        __delay_ms( 3000 );
     }
 }
 /**
